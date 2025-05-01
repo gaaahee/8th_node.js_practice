b198bd81eb7b3e7cdf882006e4d7666e7630eee2
gaaahee <lgh4155@naver.com>
2025-05-01 14:15:32 +0900

    mission: 5주차 실습 셋팅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
 import express from 'express'
+import cors from 'cors';
 
-const app = express()
-const port = 3000
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
