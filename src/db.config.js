5d5fb5e8ef5ffbb3e6d5c7f493a84d8322c96f83
gaaahee <lgh4155@naver.com>
2025-05-10 21:30:53 +0900

    chore: 사용하지 않는 패키지 정리

--- src/db.config.js
+++ src/db.config.js
@@ -1,4 +1,4 @@
-import mysql from "mysql2/promise";
+//import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import { PrismaClient } from "./generated/prisma/index.js";
 
@@ -6,13 +6,13 @@ export const prisma = new PrismaClient();
 
 dotenv.config();
 
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  port: process.env.DB_PORT || 3306,
-  database: process.env.DB_NAME || "8th_umc",
-  password: process.env.DB_PASSWORD,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// export const pool = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   port: process.env.DB_PORT || 3306,
+//   database: process.env.DB_NAME || "8th_umc",
+//   password: process.env.DB_PASSWORD,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
